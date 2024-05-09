c2674f75d976e337c8a4db7e849b53a7598d4466
AlexFromNorth_desc <jacklex.ab@gmail.com>
2024-05-09 18:02:29 +0300

    added btn restart and fix bug - mouse leave

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-var canvas = document.getElementById("canvas");
-var ball = canvas.getContext("2d");
+const canvas = document.getElementById("canvas");
+const restartBtn = document.querySelector('.restart')
+const ball = canvas.getContext("2d");
 
+function restart(){
+    ballObj.x = canvasSize.x/2
+    ballObj.y = canvasSize.y/2
+    
+    ballObj.xV = 1
+    ballObj.yV = 1
+}
+function mouseOut(){
+     mouseObj.x = -10
+     mouseObj.y = -10
+}
+
+// click to restart btn
+restartBtn.addEventListener('click',()=>restart())
+
+// default params
+const canvasSize = {x: 600, y: 600}
+const ballObj = {x: canvasSize.x/2, y: canvasSize.y/2, xV: 1, yV: 1, size: 10}
 const mouseObj = {x : 0, y : 0, xV : 0, yV : 0}
+
+canvas.width = canvasSize.x
+canvas.height = canvasSize.y
+
+ball.fillStyle = "green";
 
 // hovered mouse to the ball
 canvas.addEventListener('mousemove',(e)=>{
@@ -10,22 +34,13 @@ canvas.addEventListener('mousemove',(e)=>{
     
     mouseObj.y = e.offsetY
     mouseObj.yV = e.movementY
-
-    // console.log(e.offsetX)
-    // console.log(e.movementX)
-    // console.log(e.movementY)
-    // console.log(e.offsetY)
-
 })
 
-const canvasSize = {x: 600, y: 600}
-const ballObj = {x: 0, y: 0, xV: 1, yV: 1, size: 10}
+canvas.addEventListener('mouseout',(e)=>{
+    mouseOut()
+})
 
-canvas.width = canvasSize.x
-canvas.height = canvasSize.y
-
-ball.fillStyle = "green";
-
+// ball move
 (function drawCanvas(){
     ball.clearRect(0, 0, canvasSize.x, canvasSize.y)
 
@@ -35,19 +50,17 @@ ball.fillStyle = "green";
     ball.fillRect(ballObj.x, ballObj.y, ballObj.size, ballObj.size);
 
     // ball move
-    if( ballObj.x < 0 || ballObj.x > canvasSize.x - ballObj.size ){
+    if( ballObj.x <= 0 || ballObj.x >= canvasSize.x - ballObj.size ){
         ballObj.xV = -ballObj.xV
     }
-    if( ballObj.y < 0 || ballObj.y > canvasSize.y - ballObj.size){
+    if( ballObj.y <= 0 || ballObj.y >= canvasSize.y - ballObj.size){
         ballObj.yV = -ballObj.yV
     }
     
-
     // hovered mouse to the ball
     if(mouseObj.x >= ballObj.x && mouseObj.x <= ballObj.x + ballObj.size && mouseObj.y >= ballObj.y && mouseObj.y <= ballObj.y + ballObj.size){
         ballObj.xV = (ballObj.xV + mouseObj.xV/2)/2
         ballObj.yV = (ballObj.yV + mouseObj.yV/2)/2
-        console.log(ballObj.yV)
     } 
 
 
