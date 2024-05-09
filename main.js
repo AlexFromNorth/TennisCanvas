b3e3d097fecf6387ddf91e5787987f90f9229fbc
AlexFromNorth_desc <jacklex.ab@gmail.com>
2024-05-09 16:25:07 +0300

    created canvas border and a ball move

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var ball = canvas.getContext("2d");
 
-const ball = {x: 0, y: 0}
+const canvasSize = {x: 600, y: 600}
+const ballObj = {x: 0, y: 0, xV: 1, yV: 1, size: 10}
 
-ctx.fillStyle = "green";
-// ctx.fillRect(10, 10, 100, 100);
+canvas.width = canvasSize.x
+canvas.height = canvasSize.y
 
+ball.fillStyle = "green";
 
 (function drawCanvas(){
-    ctx.clearRect(0, 0, 500, 500)
+    ball.clearRect(0, 0, canvasSize.x, canvasSize.y)
 
-    ball.x++
-    ball.y++
+    ballObj.x += ballObj.xV
+    ballObj.y += ballObj.yV
 
-    ctx.fillRect(ball.x, ball.y, 10, 10);
+    ball.fillRect(ballObj.x, ballObj.y, ballObj.size, ballObj.size);
+
+    if( ballObj.x < 0 || ballObj.x > canvasSize.x - ballObj.size ){
+        ballObj.xV = -ballObj.xV
+    }
+    if(  ballObj.y < 0 || ballObj.y > canvasSize.y - ballObj.size){
+        ballObj.yV = -ballObj.yV
+    }
 
     requestAnimationFrame(drawCanvas)
 }())
